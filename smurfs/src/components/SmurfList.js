ddc51519bd9a2f118549b3427d4fcd5f5211c0e5
Sam <Jeffcoat>
2019-11-08 11:51:23 -0600

    Smurflist refactored

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchSmurf } from '../actions';
+import React from 'react'
+import Smurf from './Smurf'
 
-
-function SmurfList(props) {
-    console.log('SmurfList', props);
-    return (
-        <>
-            <button onCLick= {() => props.dispatch(fetchSmurf())}>Get a Smurf</button>
-            {props.isFetching && <div>🚀</div>}
-            {props.error && <div>{props.error.message}</div>}
-            <ul>
-                {props.smurfs.map(s => (
-                    <li className= "smurf" key ={s.name}>{s.name}</li>
-                ))}
-            </ul>
-        </>
-    );
+const SmurfList = props => {
+    return(
+        <ul>
+            {props.smurfs.characters.map(smurf => {
+                return <Smurf key={smurf.name} smurf = {smurf}/>
+            })}
+        </ul>
+    )
 }
 
-const mapDispatchToProps = {
-    fetchSmurf
-};
+export default SmurfList
 
-export default connect(state => {
-    console.log("mapStateToProps.STATE",  state);
-    return state;
-})(SmurfList);
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { fetchSmurf } from '../actions';
+
+
+// function SmurfList(props) {
+//     console.log('SmurfList', props);
+//     return (
+//         <>
+//             <button onCLick= {() => props.dispatch(fetchSmurf())}>Get a Smurf</button>
+//             {props.isFetching && <div>🚀</div>}
+//             {props.error && <div>{props.error.message}</div>}
+//             <ul>
+//                 {props.smurfs.map(s => (
+//                     <li className= "smurf" key ={s.name}>{s.name}</li>
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// }
+
+// const mapDispatchToProps = {
+//     fetchSmurf
+// };
+
+// export default connect(state => {
+//     console.log("mapStateToProps.STATE",  state);
+//     return state;
+// })(SmurfList);
