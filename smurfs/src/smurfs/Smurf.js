e16a12764d820fa1217b6084054a71d96e213ec6
Sam <Jeffcoat>
2019-11-08 15:16:30 -0600

    Further changes in smurf.js

--- smurfs/src/smurfs/Smurf.js
+++ smurfs/src/smurfs/Smurf.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
 const Smurf = props => {
-    const {name, age, height} = props.smurfs;
-    return <li>
-  Name: {name}
-    Age: {age}
-    Height: {height}</li>;
+    const {name, age, height, id} = props.smurfs;
+    return <div>
+        <h3>Name: {name}</h3>
+        <p>Age: {age}</p>
+        <p>Height: {height}</p>
+    </div>;
 };
 
 export default Smurf; 
