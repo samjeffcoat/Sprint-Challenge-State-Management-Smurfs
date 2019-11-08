a75d86def01763fafbe3a7cd1f41dc80795b7d2f
Sam <Jeffcoat>
2019-11-08 15:22:51 -0600

    SmurfList updated

--- smurfs/src/smurfs/SmurfList.js
+++ smurfs/src/smurfs/SmurfList.js
@@ -1,52 +1,57 @@
-/*import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 import Smurf from './Smurf'
 
 const SmurfList = props => {
     return(
-        <ul>
-            {props.smurfs.characters.map(smurf => {
-                return <Smurf key={smurf.name} smurf = {smurf}/>
-            })}
-        </ul>
+        <div>
+            {props.smurfs.map(smurf => (
+                <Smurf smurf={smurf} key={smurf.id}/>
+            ))}
+        </div>
     )
 }
 
-export default SmurfList
+const mapStateToProps = state => ({
+    smurfs: state.smurfs
+})
+
+export default connect(mapStateToProps)(SmurfList);
 
 
-*/
 
 
 
 
 
 
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchSmurf } from '../actions';
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { fetchSmurf } from '../actions';
 
 
-function SmurfList(props) {
-    console.log('SmurfList', props);
-    return (
-        <>
-            <button onClick= {() => props.dispatch(fetchSmurf())}>Get a Smurf</button>
-            {props.isFetching && <div>🚀</div>}
-            {props.error && <div>{props.error.message}</div>}
-            <ul>
-                {props.smurfs.map(s => (
-                    <li className= "smurf" key ={s.name}>{s.name}</li>
-                ))}
-            </ul>
-        </>
-    );
-}
+// function SmurfList(props) {
+//     console.log('SmurfList', props);
+//     return (
+//         <>
+//             <button onClick= {() => props.dispatch(fetchSmurf())}>Get a Smurf</button>
+//             {props.isFetching && <div>🚀</div>}
+//             {props.error && <div>{props.error.message}</div>}
+//             <ul>
+//                 {props.smurfs.map(s => (
+//                     <li className= "smurf" key ={s.name}>{s.name}</li>
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// }
 
-const mapDispatchToProps = {
-    fetchSmurf
-};
+// const mapDispatchToProps = {
+//     fetchSmurf
+// };
 
-export default connect(state => {
-    console.log("mapStateToProps.STATE",  state);
-    return state;
-})(SmurfList);
+// export default connect(state => {
+//     console.log("mapStateToProps.STATE",  state);
+//     return state;
+// })(SmurfList);
