e56bf014414add0eae083b3d9420bc5c2bbd0b89
Sam <Jeffcoat>
2019-11-08 10:28:31 -0600

    Sets up index.js, will need to create reducers to import

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from 'redux-thunk'; 
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+const store= createStore();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider >, document.getElementById("root"));
