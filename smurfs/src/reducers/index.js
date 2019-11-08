2cf14c2a44391d0e557d9f998979b81d4ea5e294
Sam <Jeffcoat>
2019-11-08 11:12:31 -0600

    Imports our actions into our reducers folder with some notes added

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,11 +1,18 @@
+import {
+    FETCHING_SMURF_FAILED,
+    FETCHING_SMURF_LOADING,
+    FETCHING_SMURF_SUCCESS
+} from '../actions';
+//brings in our actions
 
-
+//sets our initial state for the app
 const initialState = {
     smurfs: [],
     isFetching: false,
     error: null
 }
 
+//reducer built for initial state to get smurfs
 function reducer(state= initialState, action){
     console.log('reducer', action);
     switch(action.type){
