194f79f7b9aa21c2b77c9b71a447dbd3cac86b75
Sam <Jeffcoat>
2019-11-08 10:52:54 -0600

    Reducer built for initial state and fetching smurf

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,34 @@
 
 const initialState = {
     smurfs: [],
-    getSmurfs: false,
+    isFetching: false,
     error: null
 }
+
+function reducer(state= initialState, action){
+    console.log('reducer', action);
+    switch(action.type){
+        case FETCHING_SMURF_LOADING:
+            return {
+                ...state,
+                isFetching: true,
+                error:null
+            };
+        case FETCHING_SMURF_SUCCESS:
+            return{
+                ...state,
+                isFetching: true,
+                error : null
+            }
+        case FETCHING_SMURF_FAILED:
+            return {
+                ...state,
+                smurfs: [],
+                isFetching:false,
+                error: action.payload
+            }
+            default: 
+            return state; 
+    }
+}
+export default reducer;
